c61ee130d6ca587225b3c5abcf7e597c4b883e6d
shihk <shihkster1015@gmail.com>
2019-11-14 13:57:53 -0800

    authentication verification

--- public/firebase.js
+++ public/firebase.js
@@ -1,15 +1,14 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBsKdNN4aBDxYXzd62lLmqqRxPi9hCRdiI",
-    authDomain: "experiencepeerreview.firebaseapp.com",
-    databaseURL: "https://experiencepeerreview.firebaseio.com",
-    projectId: "experiencepeerreview",
-    storageBucket: "experiencepeerreview.appspot.com",
-    messagingSenderId: "975792372955",
-    appId: "1:975792372955:web:55cff2d32ee6185b3fe0fe"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// ref to firestore database
-var db = firebase.firestore();
-
+        var firebaseConfig = {
+            apiKey: "AIzaSyBsKdNN4aBDxYXzd62lLmqqRxPi9hCRdiI",
+            authDomain: "experiencepeerreview.firebaseapp.com",
+            databaseURL: "https://experiencepeerreview.firebaseio.com",
+            projectId: "experiencepeerreview",
+            storageBucket: "experiencepeerreview.appspot.com",
+            messagingSenderId: "975792372955",
+            appId: "1:975792372955:web:55cff2d32ee6185b3fe0fe"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        // ref to firestore database
+        var db = firebase.firestore();
